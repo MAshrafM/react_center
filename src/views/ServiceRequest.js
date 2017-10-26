22496d69cf1b349f514437a4bfa5d76a10b22c93
MAshraf <ee.mashraf@gmail.com>
2017-10-26 16:02:53 +0200

    server uploads

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -118,6 +118,18 @@ class ServiceRequest extends Component {
       })
     }
   }
+
+  handleFormData = () => {
+    const data = new FormData()
+    for (const [key, val] of Object.entries(this.state)) data.append(key, val)
+    for (const file of this.uploadInput.files) data.append('file', file)
+
+    fetch('/uploads', {
+      method: 'post',
+      body: data
+    }).then(response => {})
+  }
+
   render() {
     const fileValue = this.state.fileInput || 'Select a file to upload'
     const SingleLineField = (label, index) => (
@@ -177,9 +189,13 @@ class ServiceRequest extends Component {
                 <span>Upload Files</span>
                 <input
                   id="upload"
+                  name="upload[]"
                   type="file"
                   multiple
                   onChange={this.handleFilePath}
+                  ref={input => {
+                    this.uploadInput = input
+                  }}
                 />
               </div>
               <div className="file-path-wrapper">
@@ -203,7 +219,11 @@ class ServiceRequest extends Component {
             )}
           </div>
           <div className="col s12">
-            <RaisedButton label="Submit" primary />
+            <RaisedButton
+              label="Submit"
+              onClick={this.handleFormData}
+              primary
+            />
             <Checkbox
               label={
                 <span>
