4268c3f642ba0e65de55046d3e3054bb54cb0def
MAshraf <ee.mashraf@gmail.com>
2017-11-04 16:53:32 +0200

    masonry

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -1,38 +1,80 @@
 import React, { Component } from 'react'
+import Masonry from 'react-masonry-component'
 import { GenericCard } from './../components/GenericCard'
-import { CardTitle } from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
+import '../styles/logos.scss'
 
 class Logos extends Component {
+  state = {
+    activeTab: 'all',
+    data: [
+      { type: 'audio' },
+      { type: 'text' },
+      { type: 'video' },
+      { type: 'audio' }
+    ],
+    type: 'all'
+  }
   render() {
-    return (
-      <div className="row">
-        <div className="col m6">
-          <GenericCard
-            headerTitle="Rohit Bhati"
-            headerAvatar="http://via.placeholder.com/100x200"
-            mediaImgSrc="http://via.placeholder.com/120x100"
-            overlay={
-              <CardTitle
-                title="Nice job"
-                subtitle="Congratulations on this great Achievement"
+    const { activeTab } = this.state
+    const tabs = {
+      all: 'All',
+      audio: 'Audio',
+      video: 'Video',
+      text: 'Text'
+    }
+
+    const massonryComp = (
+      <Masonry>
+        {this.state.data
+          .filter(post => activeTab === 'all' || activeTab === post.type)
+          .map((post, i) => (
+            <div className="col s12 m6 14 x13 logo-card-container" key={i}>
+              <GenericCard
+                classes="logo-card"
+                mediaImgSrc="http://via.placeholder.com/120x100"
+                actions={
+                  <div>
+                    <FlatButton label="JPG" />
+                    <FlatButton label="PSD" />
+                  </div>
+                }
               />
-            }
-            cardTitle="Nice one"
-            cardSubtitle="Looking good"
-            actions={
-              <div>
-                <FlatButton label="Action1" />
-                <FlatButton label="Action2" />
-              </div>
-            }
-          >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
-            iusto, quis dignissimos fugiat qui architecto. Numquam illum dolor
-            officia qui sit possimus sint quisquam provident, a laudantium
-            reiciendis asperiores perferendis!
-          </GenericCard>
+            </div>
+          ))}
+      </Masonry>
+    )
+    return (
+      <div>
+        <div className="row">
+          <div className="col s12">
+            <ul className="tabs">
+              {Object.keys(tabs).map(tabKey => (
+                <li className="tab" key={tabKey + 'li'}>
+                  <a
+                    key={tabKey}
+                    href={'#' + tabKey}
+                    className={tabKey === activeTab && 'active'}
+                    onClick={() => this.setState({ activeTab: tabKey })}
+                  >
+                    {tabs[tabKey]}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+
+        {Object.keys(tabs).map(tabKey => (
+          <div
+            className="row"
+            id={tabKey}
+            key={tabKey}
+            style={{ marginBottom: 0 }}
+          >
+            {activeTab === tabKey && massonryComp}
+          </div>
+        ))}
       </div>
     )
   }
