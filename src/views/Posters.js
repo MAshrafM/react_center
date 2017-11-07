93c85dfd2389720828cd4fce3974c37a57c4d644
MAshraf <ee.mashraf@gmail.com>
2017-11-07 15:04:14 +0200

    Posters view

--- src/views/Posters.js
+++ src/views/Posters.js
@@ -1,92 +1,71 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
-import { Card, CardTitle, CardText } from 'material-ui/Card'
+import ReactDOM from 'react-dom'
+import { GenericCard } from './../components/GenericCard'
 
 class Posters extends Component {
-  state = {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      topCoord: null
+    }
+  }
+
+  componentDidMount() {
+    const thisElement = ReactDOMNode(this)
+    const top = thisElement.getBoundingClientRect().top
+    this.setState({ topCoord: top })
+  }
+
   render() {
     return (
-      <div className="container">
+      <div
+        className="container valign-wrapper"
+        style={{
+          marginBottom: 0,
+          minHeight: `calc(100vh - ${this.state.topCoord || '64'}px)`
+        }}
+      >
         <Helmet>
           <title>Posters | RCenter</title>
         </Helmet>
-        <div className="row">
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+        <div
+          className="row flow-text"
+          style={{ display: 'flex', flexWrap: 'wrap' }}
+        >
+          <h2 style={{ flex: '1 100%' }}>Poster Resources</h2>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Create Your Own">
+              Canva.com is a free graphic design tool with an easy to use
+              drag-and-drop interface and access to over a million photographs,
+              graphics, and fonts. It is used by non-designers as well as
+              professionals.
+            </GenericCard>
           </div>
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Video Tutorial">
+              Learn how to create posters in Powerpoint, Publisher, Photoshop,
+              and InDesign.
+            </GenericCard>
           </div>
-        </div>
-        <div className="row">
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Student Designs">
+              Our Students Graphic designs.
+            </GenericCard>
           </div>
 
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Professional Designs">
+              Professional Designs provided by our skilled members.
+            </GenericCard>
           </div>
-        </div>
-        <div className="row">
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Printing">
+              Our Printing Service with material prices in every shape and size.
+            </GenericCard>
           </div>
-          <div className="col s12 m6">
-            <Card>
-              <CardTitle title="Card title" subtitle="Card subtitle" />
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
-              </CardText>
-            </Card>
+          <div className="col s12 m6 flex-div">
+            <GenericCard cardTitle="Logos">Brands Logo designs.</GenericCard>
           </div>
         </div>
       </div>
