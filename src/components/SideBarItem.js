4711419cf95673b8af250e5ca59bd83de9568667
MAshraf <ee.mashraf@gmail.com>
2017-11-10 08:33:48 +0200

    refactor component

--- src/components/SideBarItem.js
+++ src/components/SideBarItem.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { ListItem } from 'material-ui/List'
 
-class SideBarItem extends Component {
-  render() {
-    return (
-      <NavLink
-        activeClassName="active"
-        exact={this.props.isExact}
-        to={this.props.linkTo}
-        key={this.props.i}
-      >
-        <ListItem
-          onTouchTap={this.props.onClick}
-          primaryText={this.props.primaryText}
-          hoverColor="#eee"
-        />
-      </NavLink>
-    )
-  }
+const SideBarItem = props => {
+  return (
+    <NavLink
+      activeClassName="active"
+      exact={props.isExact}
+      to={props.linkTo}
+      key={props.i}
+    >
+      <ListItem
+        onTouchTap={props.onClick}
+        primaryText={props.primaryText}
+        hoverColor="#eee"
+      />
+    </NavLink>
+  )
 }
 
 export default SideBarItem
