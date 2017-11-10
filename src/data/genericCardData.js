0729d2c2200e8f2f80531cdee3add975e7059a23
MAshraf <ee.mashraf@gmail.com>
2017-11-10 08:42:36 +0200

    statless data

---
+++ src/data/genericCardData.js
@@ -0,0 +1,27 @@
+export const genericCardData = [
+  {
+    title: 'Brand Manual',
+    description: 'Visual Brand Identity Manual.'
+  },
+  {
+    title: 'Logos and Posters',
+    description: 'RCenter Official Logos and Poster.'
+  },
+  {
+    title: 'Letterhead',
+    description: 'RCenter approved letterhead and letter writing guidelines.'
+  },
+  {
+    title: 'Suggest a Story',
+    description: 'Help us share your news.'
+  },
+  {
+    title: 'Service Request Form',
+    description:
+      'Let us know what you need, and we will get started on it as soon as we can.'
+  },
+  {
+    title: 'Powerpoint',
+    description: 'Official RCenter Powerpoint template.'
+  }
+]
